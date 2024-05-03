da3880c8b53626f5748995ae48442a035a98f385
S25088 <S25088@pjwstk.edu.pl>
2024-05-03 14:40:43 +0200

    Stworzenie kontrolera

---
+++ WebApplication2/Controllers/WarehouseController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Dto;
+using WebApplication2.Services;
+
+namespace WebApplication2.Controllers;
+
+
+[ApiController]
+[Route("/api/[controller]")]
+public class WarehouseController : ControllerBase
+{
+    private readonly IWarehouseService _warehouseService;
+    public WarehouseController(IWarehouseService warehouseService)
+    {
+        _warehouseService = warehouseService;
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> RegisterProductInWarehouseAsync([FromBody] RegisterProductInWarehouseRequestDTO dto)
+    {
+        try
+        {
+            var idProductWarehouse = await _warehouseService.RegisterProductInWarehouseAsync(dto);
+            return Ok(idProductWarehouse);
+        }
+        catch (Exception e)
+        {
+            return NotFound();
+        }
+        
+    }
+}
