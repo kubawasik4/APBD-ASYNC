15484f8a977d8411d6a5f222907ac16df6935a45
S25088 <S25088@pjwstk.edu.pl>
2024-05-03 15:11:46 +0200

    Dodanie metody sprawdzajacej czy produkt istnieje

--- WebApplication2/Repositores/WarehouseRepository.cs
+++ WebApplication2/Repositores/WarehouseRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace WebApplication2.Repositores;
+﻿using System.Data.SqlClient;
+
+namespace WebApplication2.Repositores;
 public interface IWarehouseRepository
 {
     public Task<int?> RegisterProductInWarehouseAsync(int idWarehouse, int idProduct, int idOrder, DateTime createdAt);
     public Task RegisterProductInWarehouseByProcedureAsync(int idWarehouse, int idProduct, DateTime createdAt);
+
+    public Task<bool> CheckProductExistenceAsync(int idProduct);
 }
 
 public class WarehouseRepository : IWarehouseRepository
@@ -11,6 +15,24 @@ public class WarehouseRepository : IWarehouseRepository
     public WarehouseRepository(IConfiguration configuration)
     {
         _configuration = configuration;
+    }
+    public async Task<bool> CheckProductExist(int idProduct)
+    {
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+        
+        await using var transaction = await connection.BeginTransactionAsync();
+        var query = "SELECT COUNT(*) FROM Product WHERE IdProduct = @IdProduct";
+        await using var checkProductCmd = new SqlCommand(query);
+        checkProductCmd.Transaction = (SqlTransaction)transaction;
+        checkProductCmd.Parameters.AddWithValue("@IdProduct", idProduct);
+        var counter = (int)await checkProductCmd.ExecuteScalarAsync();
+        if (counter > 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public async Task<int?> RegisterProductInWarehouseAsync(int idWarehouse, int idProduct, int idOrder,
