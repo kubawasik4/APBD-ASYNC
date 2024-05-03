b3a6990cb8fae23e8f7499dd977a4b179987096b
S25088 <S25088@pjwstk.edu.pl>
2024-05-03 14:49:29 +0200

    stworzenie warehouse repository

---
+++ WebApplication2/Repositores/WarehouseRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication2.Repositores;
+public interface IWarehouseRepository
+{
+    public Task<int?> RegisterProductInWarehouseAsync(int idWarehouse, int idProduct, int idOrder, DateTime createdAt);
+    public Task RegisterProductInWarehouseByProcedureAsync(int idWarehouse, int idProduct, DateTime createdAt);
+}
+
+public class WarehouseRepository : IWarehouseRepository
+{
+    private readonly IConfiguration _configuration;
+    public WarehouseRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<int?> RegisterProductInWarehouseAsync(int idWarehouse, int idProduct, int idOrder,
+        DateTime createdAt)
+    {
+        return null;
+    }
+
+    public async Task RegisterProductInWarehouseByProcedureAsync(int idWarehouse, int idProduct, DateTime createdAt)
+    {
+        return;
+    }
+
+}
