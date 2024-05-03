887546923876f698d2858e0babaaf0b75201a7c7
S25088 <S25088@pjwstk.edu.pl>
2024-05-03 14:36:53 +0200

    dodanie wareHouse Service

---
+++ WebApplication2/Services/WarehouseService.cs
@@ -0,0 +1,22 @@
+﻿using WebApplication2.Dto;
+
+namespace WebApplication2.Services;
+public interface IWarehouseService
+{
+    public Task<int> RegisterProductInWarehouseAsync(RegisterProductInWarehouseRequestDTO dto);
+}
+public class WarehouseService : IWarehouseService
+{
+    private readonly IWarehouseRepository _warehouseRepository;
+    // private readonly IProductRepository _productRepository;
+    public WarehouseService(IWarehouseRepository warehouseRepository)
+    {
+        _warehouseRepository = warehouseRepository;
+    }
+    
+    public async Task<int> RegisterProductInWarehouseAsync(RegisterProductInWarehouseRequestDTO dto)
+    {
+
+        
+    }
+}
